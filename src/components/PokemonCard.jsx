f0cfe0ed1c02d32c520247b4b6db36beae6421fe
Gaetan-Bourrier <gaetan.bourrier.pro@gmail.com>
2023-03-28 18:05:20 +0200

    React basic03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,22 @@
 function PokemonCard() {
-   
-return <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizar" />
+   const pokemon = pokemonList[1];
+   const blabla = pokemon.imgSrc ? <img src={pokemon.imgSrc} alt= "bulbasaur"/> : <p>???</p>;
+return <figure>{blabla}</figure>;
     <figcaption>bulbasaur</figcaption>
-</figure>;
+
 
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+    
+  },
+];
 
 export default PokemonCard;
